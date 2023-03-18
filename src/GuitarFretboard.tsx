accf4e7da17a832c9874d31bc9c3a33f168efe32
Mike Cullimore <michael.cullimore11@gmail.com>
2023-03-18 21:02:36 +0000

    Improve styling, add readme.

--- src/GuitarFretboard.tsx
+++ src/GuitarFretboard.tsx
@@ -1,22 +1,19 @@
-// TODO: move this to readme, prioritise.
 // TODO: what is state, what is props? # strings as prop, current notes as state?
-// TODO: fit to screen (what are viewport units?)
-// TODO: semantic input e.g. G chord: each note has string, fret, note name and scale degree.
-// TODO: inputs are array of dots with common styling. Handle semantics elsewhere.
+// TODO: fit SVG to screen. (Why is vertical scrollbar shown? Because square?)
 // TODO: remove styling here (stroke: black), do via CSS.
-// TODO: dot at each fret position.
 // TODO: ensure marker width is less than smallest fret spacing and smallest string spacing.
-// TODO: animate (input is array of arrays of positions ... also with timings?).
 // TODO: fretboard dots/markers.
+// TODO: thicker line for nut.
 // TODO: capture string identities (tuning) here?
+// TODO: add padding.
 // TODO: string thicknesses.
-// TODO: realistic fret spacing (log?).
-// TODO: common base component for fretboard, call with # strings, # frets for guitars, bass, ukelele.
+// TODO: calculate constants once.
 
 import { Position } from "./chords";
 
 const numStrings = 6;
 const numFrets = 20; // TODO: same # frets as my guitar.
+const padding = 5;
 
 interface FretboardProps {
     positions: Position[]
@@ -27,38 +24,45 @@ const getArrayOfLength = (length: number): number[] => {
     return Array.from(Array(length).keys());
 }
 
-const getFretXPosition = (fret: number): string => {
-    return `${100*fret/numFrets}%`;
+const getFretXPosition = (fret: number): number => {
+    // Based on measurements of a strat.
+    // TODO: include padding left and right.
+    const c2 = -0.13827529;
+    const c1 = 7.52403813;
+    return c1*fret + c2*fret*fret;
 }
 
-const getStringYPosition = (string: number): string => {
-    return `${10*string}%`;
+const getStringYPosition = (string: number): number => {
+    const stringSpacing = getFretXPosition(numFrets) - getFretXPosition(numFrets - 1);
+    return padding + stringSpacing*string;
 }
 
 const renderFrets = (numFrets: number): JSX.Element[] => {
     const y1 = getStringYPosition(1);
     const y2 = getStringYPosition(numStrings);
-    return getArrayOfLength(numFrets).map((_, index) => {
-        const x = getFretXPosition(index + 1)
-        return <line key={index} x1={x} y1={y1} x2={x} y2={y2} stroke="gray" />;
+    return getArrayOfLength(numFrets + 1).map((_, index) => {
+        const x = getFretXPosition(index)
+        return <line key={index} x1={x} y1={y1} x2={x} y2={y2} stroke="gray" strokeWidth={.4} strokeLinecap="round"/>;
     });
 }
 
 const renderStrings = (numStrings: number): JSX.Element[] => {
+    const x1 = getFretXPosition(0);
+    const x2 = getFretXPosition(numFrets);
     return getArrayOfLength(numStrings).map((_, index) => {
         const y = getStringYPosition(index + 1);
-        return <line key={index} x1="5%" y1={y} x2="95%" y2={y} stroke="black" />
+        return <line key={index} x1={x1} y1={y} x2={x2} y2={y} stroke="black" strokeWidth={.4} strokeLinecap="round"/>
     });
 }
 
 const renderNotes = (positions: Position[]): JSX.Element[] => {
-    // TODO: draw circle at (X - markerWidth/2, Y).
     // TODO: handle open strings (change string colour? Or hollow marker at fret 0?) 
     // TODO: handle strings not played (change string colour? Or X marker at fret 0?)
     return positions.map((position, index) => {
-        const x = getFretXPosition(position.fret);
+        const r = 1;
+        const x = getFretXPosition(position.fret) - r;
         const y = getStringYPosition(position.string);
-        return <circle key={index} cx={x} cy={y} r="2%" fill="blue" />
+        return <circle key={index} cx={x} cy={y} r={r} fill="blue" />
     });
 }
 
